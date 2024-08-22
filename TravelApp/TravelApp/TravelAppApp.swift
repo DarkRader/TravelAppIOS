63bcd9fbdf36a725f709b0098ed118dd9f3b0858
Artem Kuznetsov <artyom.20century@gmail.com>
2024-08-22 17:52:29 +0200

    ✅[Add] "Init project"

---
+++ TravelApp/TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by DarkRader on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
